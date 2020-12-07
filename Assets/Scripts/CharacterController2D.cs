b779eb44bf260de520aa535981059301bbe6c2f9
DeplorableMountaineer <tdvance@deplorablemountaineer.com>
2020-12-07 14:10:16 -0500

    Added double jump

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -2,271 +2,303 @@
 using System.Collections;
 using UnityEngine.SceneManagement; // include so we can load new scenes
 
-public class CharacterController2D : MonoBehaviour {
+public class CharacterController2D : MonoBehaviour
+{
+    // player controls
+    [Range(0.0f, 10.0f)] // create a slider in the editor and set limits on moveSpeed
+    public float moveSpeed = 3f;
 
-	// player controls
-	[Range(0.0f, 10.0f)] // create a slider in the editor and set limits on moveSpeed
-	public float moveSpeed = 3f;
+    public float jumpForce = 600f;
 
-	public float jumpForce = 600f;
+    // player health
+    public int playerHealth = 1;
 
-	// player health
-	public int playerHealth = 1;
+    // LayerMask to determine what is considered ground for the player
+    public LayerMask whatIsGround;
 
-	// LayerMask to determine what is considered ground for the player
-	public LayerMask whatIsGround;
+    // Transform just below feet for checking if player is grounded
+    public Transform groundCheck;
 
-	// Transform just below feet for checking if player is grounded
-	public Transform groundCheck;
+    // player can move?
+    // we want this public so other scripts can access it but we don't want to show in editor as it might confuse designer
+    [HideInInspector] public bool playerCanMove = true;
 
-	// player can move?
-	// we want this public so other scripts can access it but we don't want to show in editor as it might confuse designer
-	[HideInInspector]
-	public bool playerCanMove = true;
+    // SFXs
+    public AudioClip coinSfx;
+    public AudioClip deathSfx;
+    public AudioClip fallSfx;
+    public AudioClip jumpSfx;
+    public AudioClip victorySfx;
 
-	// SFXs
-	public AudioClip coinSFX;
-	public AudioClip deathSFX;
-	public AudioClip fallSFX;
-	public AudioClip jumpSFX;
-	public AudioClip victorySFX;
+    // private variables below
 
-	// private variables below
+    // store references to components on the gameObject
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private AudioSource _audio;
 
-	// store references to components on the gameObject
-	Transform _transform;
-	Rigidbody2D _rigidbody;
-	Animator _animator;
-	AudioSource _audio;
+    // hold player motion in this timestep
+    private float _velocityX;
+    private float _velocityY;
 
-	// hold player motion in this timestep
-	float _vx;
-	float _vy;
+    // player tracking
+    private bool _facingRight = true;
+    private bool _isGrounded;
+    private bool _isRunning;
+    private bool _canDoubleJump;
 
-	// player tracking
-	bool facingRight = true;
-	bool isGrounded = false;
-	bool isRunning = false;
+    // store the layer the player is on (setup in Awake)
+    int _playerLayer;
 
-	// store the layer the player is on (setup in Awake)
-	int _playerLayer;
+    // number of layer that Platforms are on (setup in Awake)
+    int _platformLayer;
+    private static readonly int Grounded = Animator.StringToHash("Grounded");
+    private static readonly int Running = Animator.StringToHash("Running");
+    private static readonly int Death = Animator.StringToHash("Death");
+    private static readonly int Victory1 = Animator.StringToHash("Victory");
+    private static readonly int Respawn1 = Animator.StringToHash("Respawn");
 
-	// number of layer that Platforms are on (setup in Awake)
-	int _platformLayer;
-	
-	void Awake () {
-		// get a reference to the components we are going to be changing and store a reference for efficiency purposes
-		_transform = GetComponent<Transform> ();
-		
-		_rigidbody = GetComponent<Rigidbody2D> ();
-		if (_rigidbody==null) // if Rigidbody is missing
-			Debug.LogError("Rigidbody2D component missing from this gameobject");
-		
-		_animator = GetComponent<Animator>();
-		if (_animator==null) // if Animator is missing
-			Debug.LogError("Animator component missing from this gameobject");
-		
-		_audio = GetComponent<AudioSource> ();
-		if (_audio==null) { // if AudioSource is missing
-			Debug.LogWarning("AudioSource component missing from this gameobject. Adding one.");
-			// let's just add the AudioSource component dynamically
-			_audio = gameObject.AddComponent<AudioSource>();
-		}
+    private void Awake()
+    {
+        // get a reference to the components we are going to be changing and store a reference for efficiency purposes
+        _transform = GetComponent<Transform>();
 
-		// determine the player's specified layer
-		_playerLayer = this.gameObject.layer;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        if (_rigidbody == null) // if Rigidbody is missing
+            Debug.LogError("Rigidbody2D component missing from this gameobject");
 
-		// determine the platform's specified layer
-		_platformLayer = LayerMask.NameToLayer("Platform");
-	}
+        _animator = GetComponent<Animator>();
+        if (_animator == null) // if Animator is missing
+            Debug.LogError("Animator component missing from this gameobject");
 
-	// this is where most of the player controller magic happens each game event loop
-	void Update()
-	{
-		// exit update if player cannot move or game is paused
-		if (!playerCanMove || (Time.timeScale == 0f))
-			return;
+        _audio = GetComponent<AudioSource>();
+        if (_audio == null)
+        {
+            // if AudioSource is missing
+            Debug.LogWarning("AudioSource component missing from this gameobject. Adding one.");
+            // let's just add the AudioSource component dynamically
+            _audio = gameObject.AddComponent<AudioSource>();
+        }
 
-		// determine horizontal
-		// velocity change based on the horizontal input
-		_vx = Input.GetAxisRaw ("Horizontal");
+        // determine the player's specified layer
+        _playerLayer = this.gameObject.layer;
 
-		// Determine if running based on the horizontal movement
-		if (_vx != 0) 
-		{
-			isRunning = true;
-		} else {
-			isRunning = false;
-		}
+        // determine the platform's specified layer
+        _platformLayer = LayerMask.NameToLayer("Platform");
+    }
 
-		// set the running animation state
-		_animator.SetBool("Running", isRunning);
+    // this is where most of the player controller magic happens each game event loop
+    private void Update()
+    {
+        // exit update if player cannot move or game is paused
+        if (!playerCanMove || (Time.timeScale == 0f))
+            return;
 
-		// get the current vertical velocity from the rigidbody component
-		_vy = _rigidbody.velocity.y;
+        // determine horizontal
+        // velocity change based on the horizontal input
+        _velocityX = Input.GetAxisRaw("Horizontal");
 
-		// Check to see if character is grounded by raycasting from the middle of the player
-		// down to the groundCheck position and see if collected with gameobjects on the
-		// whatIsGround layer
-		isGrounded = Physics2D.Linecast(_transform.position, groundCheck.position, whatIsGround);  
+        // Determine if running based on the horizontal movement
+        _isRunning = _velocityX != 0;
 
-		// Set the grounded animation states
-		_animator.SetBool("Grounded", isGrounded);
+        // set the running animation state
+        _animator.SetBool(Running, _isRunning);
 
-		if(isGrounded && Input.GetButtonDown("Jump")) // If grounded AND jump button pressed, then allow the player to jump
-		{
-			// reset current vertical motion to 0 prior to jump
-			_vy = 0f;
-			// add a force in the up direction
-			_rigidbody.AddForce (new Vector2 (0, jumpForce));
-			// play the jump sound
-			PlaySound(jumpSFX);
-		}
-	
-		// If the player stops jumping mid jump and player is not yet falling
-		// then set the vertical velocity to 0 (he will start to fall from gravity)
-		if(Input.GetButtonUp("Jump") && _vy>0f)
-		{
-			_vy = 0f;
-		}
+        // get the current vertical velocity from the rigidbody component
+        _velocityY = _rigidbody.velocity.y;
 
-		// Change the actual velocity on the rigidbody
-		_rigidbody.velocity = new Vector2(_vx * moveSpeed, _vy);
+        // Check to see if character is grounded by raycasting from the middle of the player
+        // down to the groundCheck position and see if collected with gameobjects on the
+        // whatIsGround layer
+        _isGrounded = Physics2D.Linecast(_transform.position, groundCheck.position, whatIsGround);
 
-		// if moving up then don't collide with platform layer
-		// this allows the player to jump up through things on the platform layer
-		// NOTE: requires the platforms to be on a layer named "Platform"
-		Physics2D.IgnoreLayerCollision(_playerLayer, _platformLayer, (_vy > 0.0f)); 
-	}
+        // Set the grounded animation states
+        _animator.SetBool(Grounded, _isGrounded);
 
-	// Checking to see if the sprite should be flipped
-	// this is done in LateUpdate since the Animator may override the localScale
-	// this code will flip the player even if the animator is controlling scale
-	void LateUpdate()
-	{
-		// get the current scale
-		Vector3 localScale = _transform.localScale;
+        if (_isGrounded) _canDoubleJump = true;
 
-		if (_vx > 0) // moving right so face right
-		{
-			facingRight = true;
-		} else if (_vx < 0) { // moving left so face left
-			facingRight = false;
-		}
+        if (_isGrounded && Input.GetButtonDown("Jump")
+        ) // If grounded AND jump button pressed, then allow the player to jump
+        {
+            DoJump();
+        }
+        else if (_canDoubleJump && Input.GetButtonDown("Jump")
+        ) // If can double jump AND jump button pressed, then allow the player to jump
+        {
+            _canDoubleJump = false;
+            DoJump();
+        }
 
-		// check to see if scale x is right for the player
-		// if not, multiple by -1 which is an easy way to flip a sprite
-		if (((facingRight) && (localScale.x<0)) || ((!facingRight) && (localScale.x>0))) {
-			localScale.x *= -1;
-		}
+        // If the player stops jumping mid jump and player is not yet falling
+        // then set the vertical velocity to 0 (he will start to fall from gravity)
+        if (Input.GetButtonUp("Jump") && _velocityY > 0f)
+        {
+            _velocityY = 0f;
+        }
 
-		// update the scale
-		_transform.localScale = localScale;
-	}
+        // Change the actual velocity on the rigidbody
+        _rigidbody.velocity = new Vector2(_velocityX * moveSpeed, _velocityY);
 
-	// if the player collides with a MovingPlatform, then make it a child of that platform
-	// so it will go for a ride on the MovingPlatform
-	void OnCollisionEnter2D(Collision2D other)
-	{
-		if (other.gameObject.tag=="MovingPlatform")
-		{
-			this.transform.parent = other.transform;
-		}
-	}
+        // if moving up then don't collide with platform layer
+        // this allows the player to jump up through things on the platform layer
+        // NOTE: requires the platforms to be on a layer named "Platform"
+        Physics2D.IgnoreLayerCollision(_playerLayer, _platformLayer, (_velocityY > 0.0f));
+    }
 
-	// if the player exits a collision with a moving platform, then unchild it
-	void OnCollisionExit2D(Collision2D other)
-	{
-		if (other.gameObject.tag=="MovingPlatform")
-		{
-			this.transform.parent = null;
-		}
-	}
+    private void DoJump()
+    {
+        // reset current vertical motion to 0 prior to jump
+        _velocityY = 0f;
+        // add a force in the up direction
+        _rigidbody.AddForce(new Vector2(0, jumpForce));
+        // play the jump sound
+        PlaySound(jumpSfx);
+    }
 
-	// do what needs to be done to freeze the player
- 	void FreezeMotion() {
-		playerCanMove = false;
-        _rigidbody.velocity = new Vector2(0,0);
-		_rigidbody.isKinematic = true;
-	}
+    // Checking to see if the sprite should be flipped
+    // this is done in LateUpdate since the Animator may override the localScale
+    // this code will flip the player even if the animator is controlling scale
+    void LateUpdate()
+    {
+        // get the current scale
+        Vector3 localScale = _transform.localScale;
 
-	// do what needs to be done to unfreeze the player
-	void UnFreezeMotion() {
-		playerCanMove = true;
-		_rigidbody.isKinematic = false;
-	}
+        if (_velocityX > 0) // moving right so face right
+        {
+            _facingRight = true;
+        }
+        else if (_velocityX < 0)
+        {
+            // moving left so face left
+            _facingRight = false;
+        }
 
-	// play sound through the audiosource on the gameobject
-	void PlaySound(AudioClip clip)
-	{
-		_audio.PlayOneShot(clip);
-	}
+        // check to see if scale x is right for the player
+        // if not, multiple by -1 which is an easy way to flip a sprite
+        if (((_facingRight) && (localScale.x < 0)) || ((!_facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
 
-	// public function to apply damage to the player
-	public void ApplyDamage (int damage) {
-		if (playerCanMove) {
-			playerHealth -= damage;
+        // update the scale
+        _transform.localScale = localScale;
+    }
 
-			if (playerHealth <= 0) { // player is now dead, so start dying
-				PlaySound(deathSFX);
-				StartCoroutine (KillPlayer ());
-			}
-		}
-	}
+    // if the player collides with a MovingPlatform, then make it a child of that platform
+    // so it will go for a ride on the MovingPlatform
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MovingPlatform"))
+        {
+            this.transform.parent = other.transform;
+        }
+    }
 
-	// public function to kill the player when they have a fall death
-	public void FallDeath () {
-		if (playerCanMove) {
-			playerHealth = 0;
-			PlaySound(fallSFX);
-			StartCoroutine (KillPlayer ());
-		}
-	}
+    // if the player exits a collision with a moving platform, then unchild it
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("MovingPlatform"))
+        {
+            this.transform.parent = null;
+        }
+    }
 
-	// coroutine to kill the player
-	IEnumerator KillPlayer()
-	{
-		if (playerCanMove)
-		{
-			// freeze the player
-			FreezeMotion();
+    // do what needs to be done to freeze the player
+    void FreezeMotion()
+    {
+        playerCanMove = false;
+        _rigidbody.velocity = new Vector2(0, 0);
+        _rigidbody.isKinematic = true;
+    }
 
-			// play the death animation
-			_animator.SetTrigger("Death");
-			
-			// After waiting tell the GameManager to reset the game
-			yield return new WaitForSeconds(2.0f);
+    // do what needs to be done to unfreeze the player
+    void UnFreezeMotion()
+    {
+        playerCanMove = true;
+        _rigidbody.isKinematic = false;
+    }
 
-			if (GameManager.gm) // if the gameManager is available, tell it to reset the game
-				GameManager.gm.ResetGame();
-			else // otherwise, just reload the current level
-				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-		}
-	}
+    // play sound through the audiosource on the gameobject
+    void PlaySound(AudioClip clip)
+    {
+        _audio.PlayOneShot(clip);
+    }
 
-	public void CollectCoin(int amount) {
-		PlaySound(coinSFX);
+    // public function to apply damage to the player
+    public void ApplyDamage(int damage)
+    {
+        if (playerCanMove)
+        {
+            playerHealth -= damage;
 
-		if (GameManager.gm) // add the points through the game manager, if it is available
-			GameManager.gm.AddPoints(amount);
-	}
+            if (playerHealth <= 0)
+            {
+                // player is now dead, so start dying
+                PlaySound(deathSfx);
+                StartCoroutine(KillPlayer());
+            }
+        }
+    }
 
-	// public function on victory over the level
-	public void Victory() {
-		PlaySound(victorySFX);
-		FreezeMotion ();
-		_animator.SetTrigger("Victory");
+    // public function to kill the player when they have a fall death
+    public void FallDeath()
+    {
+        if (playerCanMove)
+        {
+            playerHealth = 0;
+            PlaySound(fallSfx);
+            StartCoroutine(KillPlayer());
+        }
+    }
 
-		if (GameManager.gm) // do the game manager level compete stuff, if it is available
-			GameManager.gm.LevelCompete();
-	}
+    // coroutine to kill the player
+    IEnumerator KillPlayer()
+    {
+        if (playerCanMove)
+        {
+            // freeze the player
+            FreezeMotion();
 
-	// public function to respawn the player at the appropriate location
-	public void Respawn(Vector3 spawnloc) {
-		UnFreezeMotion();
-		playerHealth = 1;
-		_transform.parent = null;
-		_transform.position = spawnloc;
-		_animator.SetTrigger("Respawn");
-	}
+            // play the death animation
+            _animator.SetTrigger(Death);
+
+            // After waiting tell the GameManager to reset the game
+            yield return new WaitForSeconds(2.0f);
+
+            if (GameManager.gm) // if the gameManager is available, tell it to reset the game
+                GameManager.gm.ResetGame();
+            else // otherwise, just reload the current level
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void CollectCoin(int amount)
+    {
+        PlaySound(coinSfx);
+
+        if (GameManager.gm) // add the points through the game manager, if it is available
+            GameManager.gm.AddPoints(amount);
+    }
+
+    // public function on victory over the level
+    public void Victory()
+    {
+        PlaySound(victorySfx);
+        FreezeMotion();
+        _animator.SetTrigger(Victory1);
+
+        if (GameManager.gm) // do the game manager level compete stuff, if it is available
+            GameManager.gm.LevelCompete();
+    }
+
+    // public function to respawn the player at the appropriate location
+    public void Respawn(Vector3 spawnloc)
+    {
+        UnFreezeMotion();
+        playerHealth = 1;
+        _transform.parent = null;
+        _transform.position = spawnloc;
+        _animator.SetTrigger(Respawn1);
+    }
 }
